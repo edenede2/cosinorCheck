2b83141e3c671d61a5c30db47f5aa0b0f74ab8c0
edenede2 <131803027+edenede2@users.noreply.github.com>
2024-11-13 19:04:53 +0200

    s

--- streamlitCosinor.py
+++ streamlitCosinor.py
@@ -557,7 +557,7 @@ def all_dates_plot(results, original_data, window_size, period, select_period_si
         #     ci_acrophase[i] = quadrant_adjustment(theta, np.deg2rad(ci_acrophase[i]))
         
         if half_day:
-            ci_acrophase = [x - np.pi for x in ci_acrophase]
+            ci_acrophase = [x - 180 for x in ci_acrophase]
 
         # st.write(f"CI Amplitude: {ci_amplitude}")
         st.write(f"CI Acrophase: {ci_acrophase}")
