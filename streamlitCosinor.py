6b89c8346bf6c32ec9e8dc45d62b88ec54a70ab0
edenede2 <131803027+edenede2@users.noreply.github.com>
2024-11-13 18:59:50 +0200

    s

--- streamlitCosinor.py
+++ streamlitCosinor.py
@@ -434,8 +434,8 @@ def plot_cosinor(data, original_data, window_size, date_selected, period, select
 
     acrophase = quadrant_adjustment(theta, data[2]['acrophase'])
 
-    if half_day:
-        acrophase = acrophase - np.pi
+    # if half_day:
+    #     acrophase = acrophase - np.pi
     amplitude = data[2]['amplitude']
     # if half_day:
     #     acrophase = data[2]['acrophase']
