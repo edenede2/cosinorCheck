cee1489c566384d3cfe7c546df28f8e292f90d89
edenede2 <131803027+edenede2@users.noreply.github.com>
2024-11-13 19:31:44 +0200

    s

--- streamlitCosinor.py
+++ streamlitCosinor.py
@@ -545,8 +545,8 @@ def all_dates_plot(results, original_data, window_size, period, select_period_si
         theta = results[key][2]['peaks'][0]/results[key][2]['period'] * 2 * np.pi
         acrophase = quadrant_adjustment(theta, acrophase,radian=True)
 
-        if half_day:
-            acrophase = acrophase - np.pi
+        # if half_day:
+        #     acrophase = acrophase - np.pi
         
         
         # Extract confidence intervals
