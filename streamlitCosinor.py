85b6b95b5634f1ad80aeb40ea5d42417baa38ec0
edenede2 <131803027+edenede2@users.noreply.github.com>
2024-11-17 18:00:06 +0200

    s

--- streamlitCosinor.py
+++ streamlitCosinor.py
@@ -905,6 +905,8 @@ def download_results(results, original_data, window_size, period, select_period_
             if half_day:
                 hours = hours + 12
             hours = hours % 24
+            if half_day:
+                hours = hours + 12
             minutes = int((corrected_acrophase_deg/360) * 24 * 60) % 60
             corrected_acrophase_time = f"{np.abs(days)} day(s) {np.abs(hours):02d}:{np.abs(minutes):02d}"
         else:
